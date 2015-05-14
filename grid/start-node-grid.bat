234ff2fe325003e77a8a1f65cbff0f53367e0602
azhukovich <azhukovich@serena.com>
2015-05-14 22:28:51 +0300

    minor updates

--- grid/start-node-grid.bat
+++ grid/start-node-grid.bat
@@ -1,1 +1,1 @@
-java -jar ../lib/selenium-server-standalone-2.45.0.jar -role node  -hub http://localhost:4444/grid/register -Dwebdriver.chrome.driver=lib\chromedriver.exe
+java -jar ../lib/selenium-server-standalone-2.45.0.jar -role node  -hub http://localhost:4444/grid/register -Dwebdriver.chrome.driver=../lib/chromedriver.exe
